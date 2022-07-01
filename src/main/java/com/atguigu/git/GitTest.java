8f79874f1fb0c82cbb228728068bf83cf066b7d3
xiaoyangtuo33 <2582181365@qq.com>
2022-07-01 18:57:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xiaoyangtuo33
+ * @create 2022-07-01 18:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
