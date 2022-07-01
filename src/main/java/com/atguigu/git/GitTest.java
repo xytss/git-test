369944b576cca94fe6109cf767552a1db961821a
xiaoyangtuo33 <2582181365@qq.com>
2022-07-01 19:00:48 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGit2");
     }
 }
