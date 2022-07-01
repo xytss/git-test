6234aeb0814a445ac2d671b5797ec68d18d85a76
xiaoyangtuo33 <2582181365@qq.com>
2022-07-01 19:02:49 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
         System.out.println("HelloGit2");
+        System.out.println("HelloGit3");
     }
 }
